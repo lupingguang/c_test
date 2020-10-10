c9f4b7467666284b1922c0b3913138ae0cd7dcca
lupingguang <942105626@qq.com>
2020-10-10 21:16:25 +0800

    modify bucket size to 10

--- hash_map/main.c
+++ hash_map/main.c
@@ -1,5 +1,7 @@
 #include "hash_list.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 struct q_coef
 {
     unsigned char coef;
@@ -7,8 +9,8 @@ struct q_coef
     struct hlist_node hash;
 };
 
-#define HASH_NUMBER 15
-unsigned char coef[HASH_NUMBER] = {
+#define HASH_NUMBER 10
+unsigned char coef[15] = {
     0x01, 0x02, 0x04, 0x08,0x10, 0x20, 0x40, 0x80, 0x1d, 0x3a, 0x74, 0xe8, 0xce, 0x87, 0x13,
 };
 struct q_coef q_coef_list[HASH_NUMBER];
@@ -28,17 +30,34 @@ static inline int hash_func(unsigned char k)
 static void hash_init(void)
 {
     int i, j;
+    struct q_coef *dy_q_coef = NULL;
     for (i = 0 ; i < HASH_NUMBER ; i++) {
         INIT_HLIST_HEAD(&hashtbl[i]);
         INIT_HLIST_NODE(&q_coef_list[i].hash);
-        q_coef_list[i].coef = coef[i];
-        q_coef_list[i].index = i;
+        q_coef_list[i].coef = 0xff;
+        q_coef_list[i].index = 0;
     }
-    for (i = 0 ; i < HASH_NUMBER ; i++) {
-        j = hash_func(q_coef_list[i].coef);
-        //if ( i == 6) j = 10;
-        hlist_add_head(&q_coef_list[i].hash, &hashtbl[j]);
-    }
+    for (i = 0 ; i < 15 ; i++) {
+        j = hash_func(coef[i]);
+        if(q_coef_list[j].coef == 0xff)
+        {
+	        q_coef_list[j].coef = coef[i];
+	        q_coef_list[j].index = i;
+                hlist_add_head(&q_coef_list[j].hash, &hashtbl[j]);
+		printf("j1= %d, i =%d addr:%p\n",j,i, &q_coef_list[j]);
+	}
+        else
+	{
+                dy_q_coef = (struct q_coef *)malloc(sizeof(struct q_coef));
+                dy_q_coef->coef = coef[i];
+                dy_q_coef->index = i;
+                INIT_HLIST_NODE(&(dy_q_coef->hash));
+	 	printf("j2= %d, i =%d add:%p\n",j,i,(dy_q_coef));
+		hlist_add_head(&(dy_q_coef->hash), &hashtbl[j]);
+
+	}
+     }
+    
 }
 
 static void hash_test(void)
@@ -46,8 +65,9 @@ static void hash_test(void)
     int i, j;
     struct q_coef *q;
     struct hlist_node *hn;
-    //桶定义为15大小，正好与待存数据量一致，但是哈希函数计算得到的key仍然可能冲突
-    for (i = 0 ; i < HASH_NUMBER ; i++) 
+
+    //桶定义为10大小，哈希函数计算得到的key冲突
+    for (i = 0 ; i < 15 ; i++) 
     {
         j = hash_func(coef[i]);
         //printf("key =%d, value=%x member index=%d\n",j, coef[i],i);
@@ -56,16 +76,18 @@ static void hash_test(void)
         //针对同一个tbl位置(key冲突)存储多个value情况，会打印出该key对应的所有value(该tbl位置存储的链表上所有的元素)
         //判断链表结束的条件是:node节点的next成员指向NULL(后续无节点)
         hn = hashtbl[j].first; 
-        struct hlist_node *temp_hn = NULL;
-        do
-        {
-            q = container_of(hn, struct q_coef, hash);
-            printf("found:index=%d key=0x%02x array index=%d, addr=%p\n",j,  q->coef, q->index, q);
-            temp_hn = hn;
-            hn = hn->next;
+        
+	{
+        	struct hlist_node *temp_hn = NULL;
+        	do
+        	{
+            		q = container_of(hn, struct q_coef, hash);
+            		printf("bucket-index=%d key=0x%02x value=%d addr:%p\n",j,  q->coef, q->index, q);
+            		temp_hn = hn;
+            		hn = hn->next;
+        	}
+        	while(temp_hn->next != NULL);
         }
-        while(temp_hn->next != NULL);
-
         /*内核自带的遍历宏由prefatch函数，没搞懂怎么去掉，所以下面的宏移植有问题，不可用
         hlist_for_each(hn, &hashtbl[j])
         {
